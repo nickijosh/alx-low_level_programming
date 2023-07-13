0a6cabe0b8aec697cd52ac2e383cd39e5062f1b0
Nicholas <nicholasjoshua26@gmail.com>
2023-07-13 09:16:35 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.header>
+/**
+ * main - A Program that prints a line with puts function 
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
+
