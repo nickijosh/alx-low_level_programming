9224b93cd035d6d54181878fff576901db777403
Nicholas <nicholasjoshua26@gmail.com>
2023-07-13 09:26:39 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,4 @@
  * Return: 0 (Success)
  */
 int main(void)
-{
-puts("/"Programming is like building a multilingual puzzle");
-return (0)
-}
+{puts("/"Programming is like building a multilingual puzzle");return (0)}
